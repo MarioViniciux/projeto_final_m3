d6160b88367b8cfa5308c6940a051b661ff193f2
FreitasProcopio <pp918545@gmail.com>
2025-03-01 20:10:08 -0300

    :construction: feat: Create tables - novo_servico and servico

--- creating.sql
+++ creating.sql
@@ -9,3 +9,34 @@ CREATE TABLE IF NOT EXISTS Acessibilidade (
     id INT PRIMARY KEY AUTO_INCREMENT,
     acessibilidade VARCHAR(30)
 );
+
+CREATE TABLE IF NOT EXISTS novo_servico(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    contato VARCHAR(20),
+    descricao TEXT,
+    requisitos TEXT,
+    funcionamento VARCHAR(12),
+    rua VARCHAR(50),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    cep VARCHAR(10),
+    FK_categoria INT,
+    FOREIGN KEY (FK_categoria) REFERENCES categoria(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS servico(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    contato VARCHAR(20),
+    descricao TEXT,
+    requisitos TEXT,
+    funcionamento VARCHAR(20),
+    status ENUM("Ativo", "Inativo"),
+    rua VARCHAR(50),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    cep VARCHAR(10),
+    FK_categoria INT,
+    FOREIGN KEY (FK_categoria) REFERENCES categoria(id) ON DELETE CASCADE
+);
