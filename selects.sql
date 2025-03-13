c145ab3c782ca3bc1f71f4c4dc49593ff7fcd065
Rayane Gonçalves Araújo <rayanegoncalves796@gmail.com>
2025-03-12 21:36:27 -0300

    :heavy_plus_sign: build:  2/11 commit of select

---
+++ selects.sql
@@ -0,0 +1,30 @@
+# Todos os serviços com principais informações ao lado 
+SELECT 
+    s.nome AS servico, 
+    s.contato AS contato,
+    s.descricao AS descricao,
+    s.funcionamento AS horario,
+    s.requisitos AS requerimentos,
+    s.status AS funcionando,
+    GROUP_CONCAT(a.acessibilidade SEPARATOR ', ') AS acessibilidades,
+    CONCAT(s.rua, ', ', s.numero, ',', s.bairro, ', ', s.cidade, ' - ', s.cep) AS endereco
+FROM servico s
+JOIN servico_acessibilidade sa ON s.id = sa.id_servico
+JOIN acessibilidade a ON sa.id_acessibilidade = a.id
+GROUP BY s.id;
+
+
+# Todos as indicações de serviços com principais informações ao lado 
+SELECT 
+    s.nome AS servico, 
+    s.contato AS contato,
+    s.descricao AS descricao,
+    s.funcionamento AS horario,
+    s.requisitos AS requerimentos,
+    s.status AS funcionando,
+    GROUP_CONCAT(a.acessibilidade SEPARATOR ', ') AS acessibilidades,
+    CONCAT(s.rua, ', ', s.numero, ', ', s.bairro, ', ', s.cidade, ' - ', s.cep) AS endereco
+FROM novo_servico s
+JOIN novo_servico_acessibilidade sa ON s.id = sa.id_novo_servico
+JOIN acessibilidade a ON sa.id_acessibilidade = a.id
+GROUP BY s.id;
