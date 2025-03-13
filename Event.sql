842d2a92a467974d9689ed2317215f59a2dbe484
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-03-13 10:40:28 -0300

    feat: adição do evento remover_servico_incompleto

--- Event.sql
+++ Event.sql
@@ -1,3 +1,10 @@
 CREATE EVENT IF NOT EXISTS remover_servico
 ON SCHEDULE EVERY 1 DAY
 DO CALL remover_servico_com_muita_denuncia();
+
+CREATE EVENT IF NOT EXISTS remover_servico_incompleto
+ON SCHEDULE EVERY 10 MINUTE
+DO
+BEGIN
+    DELETE FROM novo_servico_temp WHERE updated_at < (NOW() - INTERVAL 15 MINUTE);
+END $$
