dfead2fd05f4f7393c22795b4065406bc31ae753
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-03-13 10:42:29 -0300

    feat: adição do evento remover_avaliacao_incompleta

--- Event.sql
+++ Event.sql
@@ -18,4 +18,11 @@ BEGIN
     DELETE FROM denuncia_temp WHERE updated_at < (NOW() - INTERVAL 15 MINUTE);
 END $$
 
+CREATE EVENT IF NOT EXISTS remover_avaliacao_incompleta
+ON SCHEDULE EVERY 10 MINUTE
+DO
+BEGIN
+    DELETE FROM avaliacao_temp WHERE updated_at < (NOW() - INTERVAL 15 MINUTE);
+END $$
+
 DELIMITER ;;
