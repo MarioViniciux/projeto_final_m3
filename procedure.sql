3b1e88af8001f44b923dc82716ba8ba4666ec1cf
Dandara <dandaradias364@gmail.com>
2025-03-12 22:59:43 -0300

    adicinando procedure2

--- procedure.sql
+++ procedure.sql
@@ -24,16 +24,43 @@ SET feedback = p_feedback,
     fk_servico = p_fk_servico,
     updated_at = CURRENT_TIMESTAMP
     WHERE browser_id = p_browser_id_avaliacao;
-    END IF;
+END IF;
 
--- Se o registro estiver concluído, move os dados para a tabela denuncia
-IF p_concluido = TRUE THEN
-  INSERT INTO avaliacao (feedback, pontuacao, data, FK_servico)
-  SELECT feedback, pontuacao, data, FK_servico
-  FROM avaliacao_temp
-  WHERE browser_id = p_browser_id_avaliacao;
+    -- Se o registro estiver concluído, move os dados para a tabela denuncia
+    IF p_concluido = TRUE THEN
+    INSERT INTO avaliacao (feedback, pontuacao, data, FK_servico)
+    SELECT feedback, pontuacao, data, FK_servico
+    FROM avaliacao_temp
+    WHERE browser_id = p_browser_id_avaliacao;
 
--- Após mover, deleta o registro de denuncia_temp
-        DELETE FROM avaliacao_temp WHERE browser_id = p_browser_id_avaliacao;
-    END IF;
+    -- Após mover, deleta o registro de denuncia_temp
+    DELETE FROM avaliacao_temp WHERE browser_id = p_browser_id_avaliacao;
+END IF;
+END $$
+
+CREATE PROCEDURE aprovar_servico(IN servicoID INT, IN adminID INT)
+BEGIN
+    DECLARE existe INT;
+    DECLARE novoID INT;
+    
+-- Verifica se o serviço existe e está pendente
+    SELECT COUNT(*) INTO existe FROM novo_servico WHERE ID = servicoID AND status = 'pendente';
+
+IF existe = 1 THEN
+ -- Move o serviço para a tabela Serviços
+    INSERT INTO servico (FK_categoria, nome, funcionamento, requisitos, contato, descricao, cidade, bairro, rua, numero,CEP, created_at, updated_at)
+    SELECT FK_categoria, nome, funcionamento, requisitos, contato, descricao, cidade, bairro, rua, numero, CEP, created_at, NOW()
+    FROM novo_servico WHERE ID = servicoID;
+        
+    SET novoID = LAST_INSERT_ID();
+        
+    INSERT INTO servico_acessibilidade (id_servico, id_acessibilidade)
+    SELECT novoID, id_acessibilidade FROM novo_servico_acessibilidade WHERE id_novo_servico = servicoID;
+        
+ -- Atualiza o serviço como aprovado e registra o admin
+    UPDATE novo_servico
+    SET status = 'aprovado', FK_admin = adminID
+    WHERE ID = servicoID;
+
+END IF;
 END $$
