4a4d619bd90d334f350197f614383d61194cef35
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-03-13 10:41:44 -0300

    feat: adição do evento remover_denuncia_incompleta

--- Event.sql
+++ Event.sql
@@ -1,3 +1,5 @@
+DELIMITER $$
+
 CREATE EVENT IF NOT EXISTS remover_servico
 ON SCHEDULE EVERY 1 DAY
 DO CALL remover_servico_com_muita_denuncia();
@@ -8,3 +10,12 @@ DO
 BEGIN
     DELETE FROM novo_servico_temp WHERE updated_at < (NOW() - INTERVAL 15 MINUTE);
 END $$
+
+CREATE EVENT IF NOT EXISTS remover_denuncia_incompleta
+ON SCHEDULE EVERY 10 MINUTE
+DO
+BEGIN
+    DELETE FROM denuncia_temp WHERE updated_at < (NOW() - INTERVAL 15 MINUTE);
+END $$
+
+DELIMITER ;;
